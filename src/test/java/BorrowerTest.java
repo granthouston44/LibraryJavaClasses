dc9ce2b387101e488768afbe8911bdda4ddc127b
Grant Houston <grant.houston44@hotmail.com>
2020-01-28 18:00:35 +0000

    borrower test setup

--- src/test/java/BorrowerTest.java
+++ src/test/java/BorrowerTest.java
@@ -5,7 +5,19 @@ import static org.junit.Assert.assertEquals;
 
 public class BorrowerTest {
 
+private Borrower borrower;
+private Book watchmen;
+private Book vendetta;
 
+@Before
+    public void before(){
+    borrower = new Borrower();
+    watchmen = new Book("The Watchmen", "Alan Moore", "Graphic Novel");
+    vendetta = new Book("V For Vendetta", "Alan Moore", "Graphic Novel");
+
+}
+
+@Test
 
 
 
